57ba022385391c2471ab1b79a6b61cf1e3cb5c38
Aery <294725131@qq.com>
2023-03-31 18:05:04 +0800

    Add files via upload

---
+++ src/components/Button.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  
+  font-size: 14px;
+  height: 32px;
+  padding: 4px 15px;
+  
+  border-radius: 6px;
+  outline: none;
+  position: relative;
+  display: inline-block;
+  font-weight: 400;
+  white-space: nowrap;
+  text-align: center;
+  background-image: none;
+  background-color: transparent;
+  border: 1px solid transparent;
+  cursor: pointer;
+  user-select: none;
+  line-height: 1.5714285714285714;
+  color: rgba(0, 0, 0, 0.88);
+  transition: all 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
+
+  background-color: #ffffff;
+  border-color: #d9d9d9;
+  box-shadow: 0 2px 0 rgba(0, 0, 0, 0.02);
+`;
