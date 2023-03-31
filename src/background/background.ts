64e9b0866d1d2438dab783fbf351d15d96380b9e
Aery <294725131@qq.com>
2023-03-31 18:02:30 +0800

    Update background.ts

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,4 +1,7 @@
 import * as browser from 'webextension-polyfill';
+import { initializeStore } from '../store/store';
+
+initializeStore();
 
 browser.action.onClicked.addListener(tab => {
   console.log(tab);
