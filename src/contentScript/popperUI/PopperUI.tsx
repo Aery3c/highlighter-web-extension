a5ec568ec60716b8a3cc3f867717a68b97c5d7bc
Aery <294725131@qq.com>
2023-04-03 18:27:38 +0800

    Update PopperUI.tsx

--- src/contentScript/popperUI/PopperUI.tsx
+++ src/contentScript/popperUI/PopperUI.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Button } from '../../common/components/Button';
+import { ButtonBase } from '../../common/components/Button';
 import styled, { ThemeProvider } from 'styled-components';
 import { theme } from '../../common/theme';
+import { GithubFilled, TagFilled } from '@ant-design/icons';
+import { HighlightButton } from './buttons';
 import type { RootState } from '../../store/store';
 import type { ConnectedProps } from 'react-redux';
 
 const PopperContainer = styled.div`
+  left: 90px;
   position: absolute;
   z-index: 1000;
 `;
@@ -24,40 +27,60 @@ const Group = styled.div`
     }
   }
 `
-const ArrowStyle = `
-  position: inherit;
-  z-index: -1;
-  width: 8px;
-  height: 8px;
-  background-color: #ffffff;
-  border: 1px solid #d9d9d9;
-  box-shadow: 0 2px 0 rgba(0, 0, 0, 0.02);
-`;
+
 const Arrow = styled.div`
-  ${ArrowStyle};
   visibility: hidden;
   bottom: -4px;
   left: calc(50% - 4px);
   ::after {
-    ${ArrowStyle};
     visibility: visible;
     content: '';
     transform: rotate(45deg);
   }
+  
+  &, &::after {
+    position: inherit;
+    z-index: -1;
+    width: 8px;
+    height: 8px;
+    background-color: ${props => props.theme.colorBgContainer};
+    border: 1px solid ${props => props.theme.colorBorder};
+    box-shadow: 0 2px 0 rgba(0, 0, 0, 0.02);
+  }
 `;
-const mapState = (state: RootState) => ({ themeType: state.config.theme });
+
+const Button = styled(ButtonBase)`
+  width: 32px;
+  padding-inline-start: 0;
+  padding-inline-end: 0;
+  font-size: 14px;
+  height: 32px;
+  background-color: ${props => props.theme.colorBgContainer};
+  border-color: ${props => props.theme.colorBorder};
+  color: ${props => props.theme.colorText};
+  box-shadow: 0 2px 0 rgba(0, 0, 0, 0.02);
+`
+
+const mapState = (state: RootState) => ({
+  themeType: state.config.themeType,
+  primaryColor: state.config.primaryColor
+});
 const connector = connect(mapState);
 
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
-const PopperUI: React.FC<PropsFromRedux> = ({ themeType }) => {
-
+const PopperUI: React.FC<PropsFromRedux> = ({ themeType, primaryColor }) => {
   return (
-    <ThemeProvider theme={theme[themeType]}>
+    <ThemeProvider theme={theme?.[themeType]?.[primaryColor]}>
       <PopperContainer>
         <Group>
-          <Button>high</Button>
-          <Button>note</Button>
+          <HighlightButton />
+          <Button>
+            <TagFilled style={{ fontSize: 16 }} />
+          </Button>
+          <Button>
+            <GithubFilled style={{ fontSize: 16 }} />
+          </Button>
         </Group>
         <Arrow />
       </PopperContainer>
